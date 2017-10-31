127a34576098fc021943c578ce09fe6354d3413a
shreymisrakiet <31861681+shreymisrakiet@users.noreply.github.com>
2017-10-31 18:30:33 +0530

    Update AjaxController.php

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 
-use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Facades\DB;   
 
 use App\Http\Controllers\Controller;
 
